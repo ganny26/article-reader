391eb95d3a98da734d2b0db71910d84f01637b46
selvaganeshr <selvaganeshrajam@gmail.com>
2017-03-31 00:34:51 +0530

    sample and shuffle

--- js/shuffle.js
+++ js/shuffle.js
@@ -30,11 +30,31 @@ $(document).ready(function() {
     console.log('loaded');
     var page_src = randomUrl();
     $('#stumble-frame').attr('src', page_src);
+//    var stumbleFrame = $('#stumble-frame');
+
+// 	var script   = document.createElement("script");
+// 		script.type  = "text/javascript";
+// 		script.text   = "var x = document.getElementsByClassName('.ingredientstitle');console.log('x data',x);"; 
+// $('#stumble-frame').append(script);
+   // $('#stumble-frame').attr('srcdoc', "<html><body><script>console.log('im from iframe');</script></body></html>");
+   
 });
+
+$('#stumble-frame').load(function(){
+	console.log('page loaded...');
+
+        // var iFrameHead = window.frames["stumble-frame"].document.getElementsByTagName("head")[0];         
+        // var myscript = document.createElement('script');
+        // myscript.type = 'text/javascript';
+        // myscript.src = "var x = document.getElementsByClassName('.ingredientstitle');console.log('x data',x)"; 
+        // iFrameHead.appendChild(myscript);
+
+ });
 
 $('#load_home').click(function() {
     var page_src = randomUrl();
     $('#stumble-frame').attr('src', page_src);
+   
 });
 
 //Ingredients scroll
@@ -44,3 +64,42 @@ $('#navIng').click(function() {
         scrollTop: $("#navIng").offset().top
     }, 2000);
 });
+
+//ingredients to scroll
+$('#navIng').click(function(e) {
+	console.log('clicked')
+    e.preventDefault();
+    scrollByClass('.ingredientstitle');
+});
+
+//velocity js
+function scrollByClass(x) {
+    $(x).velocity('scroll', {
+        duration: 1500,
+        offset: -400,
+        easing: 'ease-in-out'
+    });
+}
+
+
+  // window.addEventListener( "message",
+  //         function (e) {
+  //         	console.log('message')
+  //               if(e.origin !== "http://www.archanaskitchen.com/no-bake-white-chocolate-cheesecake-recipe"){ 
+  //               	console.log('EE',e);
+  //               	console.log('Dataaa',e.data);
+  //               	return; 
+  //               } 
+                
+  //         },
+  //         false);
+
+
+// iframe = $('<iframe>');
+
+// var script   = document.createElement("script");
+// script.type  = "text/javascript";
+// script.text  = "var x = document.getElementsByClassName('.ingredientstitle');console.log('x data',x)";
+
+// iframe[0].appendChild(script);
+// iframe.appendTo('#stumble-frame');
