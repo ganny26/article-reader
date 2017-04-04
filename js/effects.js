5623bad53a3c5903d9e35e389e617f7f1797e81b
selvaganeshr <selvaganeshrajam@gmail.com>
2017-04-04 07:29:14 +0530

    js added

---
+++ js/effects.js
@@ -0,0 +1,7 @@
+let dropInstance = new Drop({
+  target: document.querySelector('.drop-target'),
+  content: 'Welcome to the future',
+  classes: 'drop-theme-arrows',
+  position: 'bottom left',
+  openOn: 'click'
+})
