791f0113c8c18192ff9b9170d5064f4c39f52794
selvaganeshr <selvaganeshrajam@gmail.com>
2017-05-02 09:17:54 +0530

    background add

--- chrome-plugin/background.js
+++ chrome-plugin/background.js
@@ -1,3 +1,6 @@
-chrome.runtime.onMessage.addListener(function(response,sender,sendResponse){
-	alert(response);
-})
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('playContent');
+  console.log(divs);
+});
+
+console.log('background loaded...');
